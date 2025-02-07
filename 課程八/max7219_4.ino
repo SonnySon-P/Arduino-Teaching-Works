0541eae668cd2c0f87afe4f2799d67bd70e01ce2
SonnySon <58923633+SonnySon-P@users.noreply.github.com>
2025-02-07 15:48:42 +0800

    Update max7219_4.ino

--- 課程八/max7219_4.ino
+++ 課程八/max7219_4.ino
@@ -21,7 +21,7 @@ void setup() {
 
 int t = 0;
 void loop() { 
-  for (int j = t % 18; j < 8 + t % 18; j++) { 
+  for (int j = t % 18; j < (8 + t % 18) && j < 18; j++) { 
     for (int i = 0; i < 8; i++) {
       if (word_matrix[i][j] == 1) 
       {
